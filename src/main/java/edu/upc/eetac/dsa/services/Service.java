7325ba02d29265bc4bad8cba784e82b00dd431a9
SergiVera <sergitus1998@gmail.com>
2018-11-15 22:06:33 +0100

    add README.md

--- src/main/java/edu/upc/eetac/dsa/services/Service.java
+++ src/main/java/edu/upc/eetac/dsa/services/Service.java
@@ -1,4 +1,20 @@
 package edu.upc.eetac.dsa.services;
 
+import io.swagger.annotations.Api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Api(value = "/text", description = "Endpoint to Text Service")
+@Path("text")
 public class Service {
+    //Hola Mundo
+    @Path("basic")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
+    }
 }
